bc39e94d8a25d0731f73ba9008f3df34dd734531
heloisethero <thero.heloise@gmail.com>
2021-01-27 12:43:03 +0100

    write output csv for one request

--- code/collect_buzzsumo_data_for_domain_names.py
+++ code/collect_buzzsumo_data_for_domain_names.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+import csv
 
 from dotenv import load_dotenv
 import requests
@@ -17,28 +18,55 @@ params = {
     'q': domain_name_example,
     'api_key': BUZZSUMO_API_KEY,
     'num_results': 100,
-    'begin_date': datetime.timestamp(datetime.strptime('2019-01-06', '%Y-%m-%d')),
-    'end_date': datetime.timestamp(datetime.strptime('2019-01-07', '%Y-%m-%d'))
+    'begin_date': datetime.timestamp(datetime.strptime('2019-01-01', '%Y-%m-%d')),
+    'end_date': datetime.timestamp(datetime.strptime('2019-01-02', '%Y-%m-%d'))
 }
 r = requests.get('https://api.buzzsumo.com/search/articles.json', params=params)
-data = r.json()
+print(r.status_code)
 
-for i in range(len(data['results'])):
-    print(
-        data['results'][1]['domain_name'],
-        datetime.fromtimestamp(data['results'][i]['published_date']),
-        data['results'][i]['url'], 
-        data['results'][i]['total_shares'],
-        data['results'][i]['total_reddit_engagements'], 
-        data['results'][i]['twitter_shares'],
-        data['results'][i]['total_facebook_shares'],
-        # data['results'][1]['facebook_shares'],
-        # data['results'][1]['facebook_likes'],
-        # data['results'][1]['facebook_comments'],
-    )
+column_names = [
+    'url',
+    'date',
+    'domain_name',
+    'total_shares',
+    'alexa_rank',
+    'pinterest_shares',
+    'total_reddit_engagements',
+    'twitter_shares',
+    'total_facebook_shares',
+    'facebook_likes',
+    'facebook_comments',
+    'facebook_shares'
+]
 
-total_pages = data['total_pages']
-print(data['total_results'])
+output_name = 'output_csv.csv'
+output_path = os.path.join('.', 'data', 'buzzsumo_domain_name', output_name)
+f = open(output_path, 'w')
+
+with f:
+
+    writer = csv.writer(f)
+    writer.writerow(column_names)
+    
+    for result in r.json()['results']:
+        row_to_add = [
+            result['url'],
+            datetime.fromtimestamp(result['published_date']),
+            result['domain_name'], 
+            result['total_shares'],
+            result['alexa_rank'],
+            result['pinterest_shares'],
+            result['total_reddit_engagements'], 
+            result['twitter_shares'],
+            result['total_facebook_shares'],
+            result['facebook_likes'],
+            result['facebook_comments'],
+            result['facebook_shares']
+        ]
+        writer.writerow(row_to_add)
+
+total_pages = r.json()['total_pages']
+print(r.json()['total_results'])
 
 # r = requests.get(
 #     'https://api.buzzsumo.com/search/articles.json', 
