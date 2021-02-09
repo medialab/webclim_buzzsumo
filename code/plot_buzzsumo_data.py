787db899ef7946623588a6b49c7db0028fcdba74
heloisethero <thero.heloise@gmail.com>
2021-02-09 12:06:40 +0100

    plot all metrics for all domain names

--- code/plot_buzzsumo_data.py
+++ code/plot_buzzsumo_data.py
@@ -1,30 +1,76 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-file_name = "established_news_cnn.csv"
-data_path = os.path.join(".", "data", "buzzsumo_domain_name", file_name)
-df = pd.read_csv(data_path)
+def plot_platform_metrics(df, domain_name=None):
 
-print(df.columns)
-df = df[df['domain_name']=='cnn.com']
+    columns_to_plot = [
+        "total_facebook_shares",
+        "twitter_shares",
+        "total_reddit_engagements",
+        "pinterest_shares",
+        "alexa_rank",
+    ]
 
-df['date'] = [datetime.fromtimestamp(x).date() for x in df['published_date']]
+    plt.figure(figsize=(10, 15))
 
-plt.figure(figsize=(10, 5))
+    for subplot_index in range(5):
 
-plt.title('CNN.com', fontsize='x-large')
+        ax = plt.subplot(5, 1, subplot_index + 1)
 
-plt.plot(df.groupby(by=["date"])["total_facebook_shares"].mean(), 
-        label="Average Facebook shares")
-plt.plot(df.groupby(by=["date"])["twitter_shares"].mean(), 
-        label="Average Twitter shares")
+        plt.plot(df.groupby(by=["date"])[columns_to_plot[subplot_index]].mean(),
+                label=columns_to_plot[subplot_index], color='C' + str(subplot_index))
+        plt.legend()
 
-plt.legend()
-plt.tight_layout()
-figure_path = os.path.join('.', 'figure', 'cnn.png')
-plt.savefig(figure_path)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.grid(axis="y")
+        plt.locator_params(axis='y', nbins=4)
+
+        plt.xlim(
+            np.datetime64(datetime.strptime('2018-12-31', '%Y-%m-%d') - timedelta(days=4)), 
+            np.datetime64(datetime.strptime('2021-01-01', '%Y-%m-%d') + timedelta(days=4))
+        )
+        plt.ylim(bottom=0)
+
+        if subplot_index == 0:
+            if domain_name:
+                plt.title(domain_name, fontsize='x-large')
+            else:
+                plt.title('Established news domain names', fontsize='x-large')
+
+    plt.tight_layout()
+
+
+def save_main_figure(domain_name=None):
+    figure_name = 'established_news.png'
+    if domain_name:
+        figure_name = 'established_news_' + domain_name + '.png'
+    figure_path = os.path.join('.', 'figure', figure_name)
+    plt.savefig(figure_path)
+    print("The '{}' figure is saved.".format(figure_name))
+
+
+if __name__=="__main__":
+
+    file_name = "established_news_2020-01-02.csv"
+    data_path = os.path.join(".", "data", "buzzsumo_domain_name", file_name)
+    df = pd.read_csv(data_path)
+
+    df['date'] = [datetime.fromtimestamp(x).date() for x in df['published_date']]
+    print(df.domain_name.unique())
+
+    plot_platform_metrics(df)
+    save_main_figure()
+
+    for domain_name in df['domain_name'].unique():
+        temp_df = df[df['domain_name']==domain_name]
+        plot_platform_metrics(temp_df, domain_name)
+        save_main_figure(domain_name.split('.')[0])
+
+
