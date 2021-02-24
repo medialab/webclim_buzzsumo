859a2b4c8f26fa0e2f514af4e428cbf79e3fbd11
heloisethero <thero.heloise@gmail.com>
2021-02-24 15:31:47 +0100

    small

--- code/concat_csvs.py
+++ code/concat_csvs.py
@@ -4,16 +4,18 @@ import pandas as pd
 import numpy as np
 
 
-df1 = pd.read_csv('./data/buzzsumo_domain_name/articles.csv')
-df2 = pd.read_csv('./data/buzzsumo_domain_name/articles_2.csv')
-df3 = pd.read_csv('./data/buzzsumo_domain_name/articles_3.csv')
-df4 = pd.read_csv('./data/buzzsumo_domain_name/articles_4.csv')
+df1 = pd.read_csv('./data/buzzsumo_domain_name/principia_2021-02-24.csv')
+df2 = pd.read_csv('./data/buzzsumo_domain_name/health_2021-02-24.csv')
+df3 = pd.read_csv('./data/buzzsumo_domain_name/misinfo_news_2021-02-24.csv')
+df4 = pd.read_csv('./data/buzzsumo_domain_name/misinfo_news_infowars_2021-02-23.csv')
+df5 = pd.read_csv('./data/buzzsumo_domain_name/sott_2021-02-24.csv')
 
-print(len(df1) + len(df2) + len(df3) + len(df4))
+print(len(df1) + len(df2) + len(df3) + len(df4) + len(df5))
 
 print()
 
-df = pd.concat([df1, df2, df3, df4])
+df = pd.concat([df1, df2, df3, df4, df5])
 print(len(df))
+print(df['domain_name'].unique())
 
-df.to_csv('./data/buzzsumo_domain_name/misinfo_news_infowars.csv', index=False)
+df.to_csv('./data/buzzsumo_domain_name/misinfo_news_2021-02-23.csv', index=False)
