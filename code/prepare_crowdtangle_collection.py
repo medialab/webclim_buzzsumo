2f6c2963dbce34228288ea5e4d19dc29abfe5907
heloisethero <thero.heloise@gmail.com>
2021-03-04 16:09:34 +0100

    small

--- code/prepare_crowdtangle_collection.py
+++ code/prepare_crowdtangle_collection.py
@@ -3,17 +3,17 @@ import os
 import pandas as pd 
 
 
-# domain_name = "breitbart.com"
-# file_name = 'misinfo_news_2021-02-24.csv'
+domain_name = "infowars.com"
+file_name = 'misinfo_news_2021-02-24.csv'
 
-domain_name = "nypost.com"
-file_name = 'established_news_2021-02-01.csv'
+# domain_name = "nypost.com"
+# file_name = 'established_news_2021-02-01.csv'
 
 input_path = os.path.join('.', 'data', 'buzzsumo_domain_name', file_name)
 df = pd.read_csv(input_path)
 
 df = df[df['domain_name'] == domain_name]
-df = df.iloc[:1000]
+# df = df.iloc[:1000]
 print(len(df))
 
 output_name = domain_name.split('.')[0] + '.csv'
