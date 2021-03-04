694e1d9f297689ed196a68e0acb431aea3530a83
heloisethero <thero.heloise@gmail.com>
2021-03-04 16:07:45 +0100

    add an article counter

--- code/collect_buzzsumo_data_for_domain_names.py
+++ code/collect_buzzsumo_data_for_domain_names.py
@@ -6,6 +6,7 @@ import time
 
 from dotenv import load_dotenv
 import requests
+import pandas as pd
 
 from domain_name_lists import ESTABLISHED_NEWS_DOMAIN_NAMES, MISINFORMATION_DOMAIN_NAMES
 from buzzsumo_columns_to_keep import BUZZSUMO_COLUMNS_TO_KEEP
@@ -15,9 +16,11 @@ if __name__=="__main__":
 
     # domain_name_list = ESTABLISHED_NEWS_DOMAIN_NAMES
     domain_name_list = MISINFORMATION_DOMAIN_NAMES
+    # domain_name_list = ['infowars.com']
 
     # We collect the data for 2019 and 2020, so 365 * 2 + 1 days as 2020 was a leap year.
     collection_period_length = 365 * 2 + 1
+    # collection_period_length = 365 * 4 + 61 + 1
 
     load_dotenv()
     params = {
@@ -29,6 +32,8 @@ if __name__=="__main__":
     output_path = os.path.join('.', 'data', 'buzzsumo_domain_name', output_name)
     f = open(output_path, 'w')
 
+    nb_df = pd.DataFrame(columns = ['date', 'article_number'])
+
     with f:
 
         writer = csv.writer(f)
@@ -39,6 +44,7 @@ if __name__=="__main__":
             params['q'] = domain_name
             print('\n\n', domain_name.upper(), '\n')
 
+            # begin_date = datetime.strptime('2017-01-01', '%Y-%m-%d')
             begin_date = datetime.strptime('2019-01-01', '%Y-%m-%d')
 
             for date_index in range(collection_period_length):
@@ -78,4 +84,12 @@ if __name__=="__main__":
                 for result in r.json()['results']:
                     writer.writerow([result[column_name] for column_name in BUZZSUMO_COLUMNS_TO_KEEP])
 
+                nb_df = nb_df.append({
+                    'date': begin_date,
+                    'article_number': int(r.json()['total_results']),
+                    }, ignore_index=True)
+
                 begin_date += timedelta(days=1)
+
+            nb_path = os.path.join('.', 'data', 'buzzsumo_domain_name', domain_name.split('.')[0] + '_nb.csv')
+            nb_df.to_csv(nb_path, index=False)
