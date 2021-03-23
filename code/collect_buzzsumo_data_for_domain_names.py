93e79eda15ea4f76493f21a77d14762bed8764b9
heloisethero <thero.heloise@gmail.com>
2021-03-23 15:38:12 +0100

    adapt code to save two csvs during long collection

--- code/collect_buzzsumo_data_for_domain_names.py
+++ code/collect_buzzsumo_data_for_domain_names.py
@@ -8,19 +8,15 @@ from dotenv import load_dotenv
 import requests
 import pandas as pd
 
-from domain_name_lists import ESTABLISHED_NEWS_DOMAIN_NAMES, MISINFORMATION_DOMAIN_NAMES
+from domain_name_lists import MISINFORMATION_DOMAIN_NAMES_2
 from buzzsumo_columns_to_keep import BUZZSUMO_COLUMNS_TO_KEEP
 
 
 if __name__=="__main__":
 
-    # domain_name_list = ESTABLISHED_NEWS_DOMAIN_NAMES
-    domain_name_list = MISINFORMATION_DOMAIN_NAMES
-    # domain_name_list = ['infowars.com']
-
+    domain_name_list = MISINFORMATION_DOMAIN_NAMES_2
     # We collect the data for 2019 and 2020, so 365 * 2 + 1 days as 2020 was a leap year.
     collection_period_length = 365 * 2 + 1
-    # collection_period_length = 365 * 4 + 61 + 1
 
     load_dotenv()
     params = {
@@ -32,64 +28,62 @@ if __name__=="__main__":
     output_path = os.path.join('.', 'data', 'buzzsumo_domain_name', output_name)
     f = open(output_path, 'w')
 
-    nb_df = pd.DataFrame(columns = ['date', 'article_number'])
+    output_2_name = sys.argv[1].split('.')[0] + '_nb.csv' if len(sys.argv) >= 2 else 'articles_nb.csv'
+    output_2_path = os.path.join('.', 'data', 'buzzsumo_domain_name', output_2_name)
+    g = open(output_2_path, 'w')
 
     with f:
 
         writer = csv.writer(f)
-        writer.writerow(BUZZSUMO_COLUMNS_TO_KEEP)
+        writer.writerow(['date'] + BUZZSUMO_COLUMNS_TO_KEEP)
 
-        for domain_name in domain_name_list:
+        with g:
+            writer_2 = csv.writer(g)
+            writer_2.writerow(['domain_name', 'date', 'article_number'])   
 
-            params['q'] = domain_name
-            print('\n\n', domain_name.upper(), '\n')
+            for domain_name in domain_name_list:
 
-            # begin_date = datetime.strptime('2017-01-01', '%Y-%m-%d')
-            begin_date = datetime.strptime('2019-01-01', '%Y-%m-%d')
+                params['q'] = domain_name
+                print('\n\n', domain_name.upper(), '\n')
 
-            for date_index in range(collection_period_length):
+                begin_date = datetime.strptime('2019-01-01', '%Y-%m-%d')
 
-                if begin_date.day == 1:
-                    print('\n  ', begin_date.strftime("%Y-%m-%d"))
-                params['begin_date'] = begin_date.timestamp()
-                params['end_date'] = (begin_date + timedelta(days=1)).timestamp()
+                for date_index in range(collection_period_length):
 
-                api_call_attempt = 0
-
-                while True:
-
-                    # if first try, add a sleep function so that we wait at least 1.05 seconds between two calls (see later code)
-                    if api_call_attempt == 0:
-                        start_call_time = time.time()
-                    # if second try or more, wait an exponential time
-                    else: 
-                        time.sleep(2**(api_call_attempt - 1))
-                    
-                    r = requests.get('https://api.buzzsumo.com/search/articles.json', params=params)
-                    status_code = r.status_code
                     if begin_date.day == 1:
-                        print('Remaining number of calls for this month:', r.headers['X-RateLimit-Month-Remaining'])
-                    print('Call status code:', status_code)
-                    
-                    if api_call_attempt == 0:
-                        end_call_time = time.time()
-                        if (end_call_time - start_call_time) < 1.2:
-                            time.sleep(1.2 - (end_call_time - start_call_time))
+                        print('\n  ', begin_date.strftime("%Y-%m-%d"))
+                    params['begin_date'] = begin_date.timestamp()
+                    params['end_date'] = (begin_date + timedelta(days=1)).timestamp()
 
-                    if status_code == 200:
-                        break
+                    api_call_attempt = 0
 
-                    api_call_attempt += 1
+                    while True:
 
-                for result in r.json()['results']:
-                    writer.writerow([result[column_name] for column_name in BUZZSUMO_COLUMNS_TO_KEEP])
+                        # if first try, add a sleep function so that we wait at least 1.05 seconds between two calls (see later code)
+                        if api_call_attempt == 0:
+                            start_call_time = time.time()
+                        # if second try or more, wait an exponential time
+                        else: 
+                            time.sleep(2**(api_call_attempt - 1))
+                        
+                        r = requests.get('https://api.buzzsumo.com/search/articles.json', params=params)
+                        status_code = r.status_code
+                        if (begin_date.day == 1) and ('X-RateLimit-Month-Remaining' in r.headers.keys()):
+                            print('Remaining number of calls for this month:', r.headers['X-RateLimit-Month-Remaining'])
+                        print('Call status code:', status_code)
+                        
+                        if api_call_attempt == 0:
+                            end_call_time = time.time()
+                            if (end_call_time - start_call_time) < 1.2:
+                                time.sleep(1.2 - (end_call_time - start_call_time))
 
-                nb_df = nb_df.append({
-                    'date': begin_date,
-                    'article_number': int(r.json()['total_results']),
-                    }, ignore_index=True)
+                        if status_code == 200:
+                            break
 
-                begin_date += timedelta(days=1)
+                        api_call_attempt += 1
 
-            nb_path = os.path.join('.', 'data', 'buzzsumo_domain_name', domain_name.split('.')[0] + '_nb.csv')
-            nb_df.to_csv(nb_path, index=False)
+                    for result in r.json()['results']:
+                        writer.writerow([begin_date.date()] + [result[column_name] for column_name in BUZZSUMO_COLUMNS_TO_KEEP])
+                    writer_2.writerow([domain_name, begin_date.date(), int(r.json()['total_results'])])
+
+                    begin_date += timedelta(days=1)
