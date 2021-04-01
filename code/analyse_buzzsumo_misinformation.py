8b92928949f495baffc22c2dd4c089b9f3726c19
heloisethero <thero.heloise@gmail.com>
2021-04-01 19:38:51 +0200

    adapt figures to more data

--- code/analyse_buzzsumo_misinformation.py
+++ code/analyse_buzzsumo_misinformation.py
@@ -23,6 +23,14 @@ def arrange_plot(ax):
         np.datetime64(datetime.strptime('2021-01-01', '%Y-%m-%d') + timedelta(days=4))
     )
 
+
+def plot_facebook_engagement(df_temp):
+    plt.plot(df_temp.resample('D', on='date')['facebook_likes'].mean().rolling(window=5, win_type='triang', center=True).mean(), label="Facebook likes per post")
+    plt.plot(df_temp.resample('D', on='date')['facebook_shares'].mean().rolling(window=5, win_type='triang', center=True).mean(), label="Facebook shares per post")
+    plt.plot(df_temp.resample('D', on='date')['facebook_comments'].mean().rolling(window=5, win_type='triang', center=True).mean(), label="Facebook comments per post")
+    plt.axvline(x=np.datetime64("2020-06-09"), color='black', linestyle='--', linewidth=1)
+
+
 def plot_average_engagement(df):
     
     plt.figure(figsize=(10, 12))
@@ -31,22 +39,19 @@ def plot_average_engagement(df):
     plt.title('Average for the {} misinformation domain names'.format(df.domain_name.nunique()), fontsize='x-large')
 
     arrange_plot(ax)
-    plt.plot(df.resample('D', on='date')['facebook_likes'].sum().rolling(window=5, win_type='triang', center=True).mean(), label="Facebook likes per day")
-    plt.plot(df.resample('D', on='date')['facebook_shares'].sum().rolling(window=5, win_type='triang', center=True).mean(), label="Facebook shares per day")
-    plt.plot(df.resample('D', on='date')['facebook_comments'].sum().rolling(window=5, win_type='triang', center=True).mean(), label="Facebook comments per day")
-    plt.axvline(x=np.datetime64("2020-06-09"), color='black', linestyle='--', linewidth=1)
+    plot_facebook_engagement(df)
     plt.legend()
 
     ax = plt.subplot(312)
     arrange_plot(ax)
-    plt.plot(df.resample('D', on='date')['twitter_shares'].sum().rolling(window=5, win_type='triang', center=True).mean(), label="Twitter shares per day", color='C3')
+    plt.plot(df.resample('D', on='date')['twitter_shares'].mean().rolling(window=5, win_type='triang', center=True).mean(), label="Twitter shares per tweet", color='C3')
     plt.axvline(x=np.datetime64("2020-06-09"), color='black', linestyle='--', linewidth=1)
     plt.legend()
 
     ax = plt.subplot(313)
     arrange_plot(ax)
     plt.plot(df.resample('D', on='date')['date'].agg('count'), 
-        label= "Number of articles published per day", color=[.2, .2, .2])
+        label="Sum of articles published per day", color=[.2, .2, .2])
     plt.legend()
 
     plt.tight_layout()
@@ -63,16 +68,17 @@ def plot_article_number_individually(df_nb):
         ax = plt.subplot(5, 2, domain_name_index % 10 + 1)
 
         plt.plot(df_nb[df_nb['domain_name']==domain_name].resample('D', on='date')['article_number'].sum(), 
-                 label=domain_name)
+                 label="Number of articles published per day", color=[.2, .2, .2])
         plt.hlines(y=0, color='black', linestyle='--', linewidth=1,
                 xmin=np.datetime64(datetime.strptime('2018-12-31', '%Y-%m-%d')), 
                 xmax=np.datetime64(datetime.strptime('2021-01-01', '%Y-%m-%d')))
         arrange_plot(ax)
+        if domain_name_index % 10 == 0: plt.legend()
         plt.title(domain_name)
 
         if (domain_name_index % 10 == 9) | (domain_name_index == df_nb.domain_name.nunique() - 1):
             plt.tight_layout()
-            save_figure('34_misinformation_article_nb_{}'.format(int(domain_name_index / 10) + 1))
+            save_figure('{}_misinformation_article_nb_{}.png'.format(df_nb.domain_name.nunique(), int(domain_name_index / 10) + 1))
 
         domain_name_index += 1
 
@@ -83,19 +89,26 @@ if __name__=="__main__":
     #    'alexa_rank', 'pinterest_shares', 'total_reddit_engagements',
     #    'twitter_shares', 'total_facebook_shares', 'facebook_likes',
     #    'facebook_comments', 'facebook_shares']
-    df_url = pd.read_csv('./data/buzzsumo_domain_name/misinformation_2021-03-23.csv')
+    df_url_1 = pd.read_csv('./data/buzzsumo_domain_name/misinformation_2021-03-23.csv')
+    df_url_2 = pd.read_csv('./data/buzzsumo_domain_name/misinformation_2021-03-30.csv')
+    df_url = pd.concat([df_url_1, df_url_2])
+
     df_url['date'] = [datetime.fromtimestamp(x).date() for x in df_url['published_date']]
     df_url['date'] = pd.to_datetime(df_url['date'])
+    df_url = df_url.drop_duplicates(subset=['url'])
 
     plot_average_engagement(df_url)
-    save_figure(figure_name='34_misinformation_engagement.png')
+    save_figure(figure_name='{}_misinformation_engagement.png'.format(df_url.domain_name.nunique()))
 
-    excluded_domain_name = ['zerohedge.com', 'stateofthenation.co', 'principia-scientific.com', 'newsbreak.com', 'infowars.com', 'humansarefree.com', 'greenmedinfo.com', 'dcdirtylaundry.com', 'dcclothesline.com', 'davidicke.com']
-    df_url = df_url[~df_url['domain_name'].isin(excluded_domain_name)]
-    plot_average_engagement(df_url)
-    save_figure(figure_name='34_misinformation_engagement_filtered.png')
+    # excluded_domain_name = ['zerohedge.com', 'stateofthenation.co', 'principia-scientific.com', 'newsbreak.com', 'infowars.com', 'humansarefree.com', 'greenmedinfo.com', 'dcdirtylaundry.com', 'dcclothesline.com', 'davidicke.com']
+    # df_url = df_url[~df_url['domain_name'].isin(excluded_domain_name)]
+    # plot_average_engagement(df_url)
+    # save_figure(figure_name='34_misinformation_engagement_filtered.png')
 
     # ['domain_name', 'date', 'article_number']
-    df_nb = pd.read_csv('./data/buzzsumo_domain_name/misinformation_2021-03-23_nb.csv')
+    df_nb_1 = pd.read_csv('./data/buzzsumo_domain_name/misinformation_2021-03-23_nb.csv')
+    df_nb_2 = pd.read_csv('./data/buzzsumo_domain_name/misinformation_2021-03-30_nb.csv')
+    df_nb = pd.concat([df_nb_1, df_nb_2])
+
     df_nb['date'] = pd.to_datetime(df_nb['date'])
     plot_article_number_individually(df_nb)
